0ef085492af02bbb7d45f0890685d78c8fd60c99
nabinbhusal80 <nabinbhusal80@gmail.com>
2018-04-26 08:40:29 +0545

    adding file to the repository
    just added a file

---
+++ facebook_base.py
@@ -0,0 +1,17 @@
+import requests
+import time
+import random
+
+token = "token-is-given-to-you-according-to-your-subscription"
+
+
+def facebook_req(req):
+	r = requests.get("https://graph.facebook.com/v2.12/"+ req, {'access_token':token})
+	return r 
+
+data = ['id','username','posts']
+half_url = "teamwinkey?fields={}".format(data)
+print(half_url)
+response = facebook_req(half_url)
+
+print(response.json())
